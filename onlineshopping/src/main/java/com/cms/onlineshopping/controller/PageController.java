e4d27ba58d87aea71cc63247b605b73ae446e883
suyog <suyog@LAPTOP-78V7SR6C>
2017-07-29 23:11:24 +0530

    front controller part 2 -5 completed

--- onlineshopping/src/main/java/com/cms/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/cms/onlineshopping/controller/PageController.java
@@ -1,7 +1,9 @@
 package com.cms.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -12,9 +14,31 @@ public class PageController {
 	public ModelAndView index(){
 		
 		 ModelAndView mv=new ModelAndView("page");
-		 mv.addObject("greeting", "welcome to cms");
+		 mv.addObject("greeting", "welcome to cms suyog malviya");
 		 return mv;
 	}
+	
+	/*@RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam(value="greeting" ,required= false) String greeting){
+		if(greeting==null){
+			greeting=" hello guys";
+		}
+		ModelAndView mv1=new ModelAndView("page");
+		mv1.addObject("greeting", greeting);
+		return mv1;
+	}
+
+	@RequestMapping(value="/gmail/{greeting}")
+	public ModelAndView gmail(@PathVariable("greeting")String greeting){
+		if(greeting==null){
+			greeting=" hello guys";
+		}
+		ModelAndView mv1=new ModelAndView("page");
+		mv1.addObject("greeting", greeting);
+		return mv1;
+	}
+*/	
+	
 	
 	
 }
--- onlineshopping/src/main/webapp/index.jsp
+++ onlineshopping/src/main/webapp/index.jsp
@@ -1,5 +1,12 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Free paay...</h2>
+
+<a href="mylogin">Login</a>&nbsp;
+ <a href="myregister">register</a>&nbsp;
+<a href="Recharge">recharge</a> 
+<a href="studentregistration">Student Registration</a> 
+
+
 </body>
 </html>
